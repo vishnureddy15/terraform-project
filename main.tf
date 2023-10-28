436964aa0b49bebf914c6e225975caf64c33c58b
vishnureddy15 <142593429+vishnureddy15@users.noreply.github.com>
2023-10-28 20:00:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ region = "ap-southeast-1"
 resource "aws_instance" "one" {
   ami             = "ami-0b0f138edf421d756"
   instance_type   = "t2.micro"
-  key_name        = "mumbai1"
+  key_name        = "munbai1"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1a"
   user_data       = <<EOF
@@ -25,7 +25,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-0b0f138edf421d756"
   instance_type   = "t2.micro"
-  key_name        = "mumbai1"
+  key_name        = "munbai1"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1b"
   user_data       = <<EOF
@@ -44,7 +44,7 @@ EOF
 resource "aws_instance" "three" {
   ami             = "ami-0b0f138edf421d756"
   instance_type   = "t2.micro"
-  key_name        = "mumbai1"
+  key_name        = "munbai1"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1a"
   tags = {
@@ -55,7 +55,7 @@ resource "aws_instance" "three" {
 resource "aws_instance" "four" {
   ami             = "ami-0b0f138edf421d756"
   instance_type   = "t2.micro"
-  key_name        = "mumbai1"
+  key_name        = "munbai1"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1b"
   tags = {
