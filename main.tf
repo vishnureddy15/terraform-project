493b8a0236621fe057c5cf92f909c3e616fcb40c
vishnureddy15 <142593429+vishnureddy15@users.noreply.github.com>
2023-10-28 17:11:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "three" {
 resource "aws_instance" "four" {
   ami             = "ami-06018068a18569ff2"
   instance_type   = "t2.micro"
-  key_name        = "swiggynewkeypairr"
+  key_name        = "mumbai"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1b"
   tags = {
