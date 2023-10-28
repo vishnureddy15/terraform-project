0e258f8c81ee2e37d1d192c7db01574e17cf9f6b
vishnureddy15 <142593429+vishnureddy15@users.noreply.github.com>
2023-10-28 17:15:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-06018068a18569ff2"
   instance_type   = "t2.micro"
-  key_name        = "swiggynewkeypairr"
+  key_name        = "mumbai"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1b"
   user_data       = <<EOF
@@ -44,7 +44,7 @@ EOF
 resource "aws_instance" "three" {
   ami             = "ami-06018068a18569ff2"
   instance_type   = "t2.micro"
-  key_name        = "swiggynewkeypairr"
+  key_name        = "mumbai"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-southeast-1a"
   tags = {
